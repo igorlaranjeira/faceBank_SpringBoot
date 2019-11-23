056499bb7f7d708672e13bb0acec1dc0f10409ba
Igor Laranjeira <igorlaranjeira1@gmail.com>
2019-11-23 19:25:44 -0300

    Implementando SpringBoot

---
+++ FacesBank/src/main/java/com/bank/FacesBank/FacesBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.FacesBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacesBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacesBankApplication.class, args);
+	}
+
+}
